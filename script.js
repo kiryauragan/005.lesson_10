a754f7a3ff84760ee535b33a977ba621f6b8a5a4
kiryauragan <you@example.comkirilltaranskyi@gmail.com>
2023-02-26 12:52:16 +0200

    script.js updated

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
-let length = prompt("Введіть довжину масиву:");
-let elements = prompt(`Введіть ${length} елементів:`)
+let length = prompt("Введіть довжину масиву:", "5");
+let elements = prompt(`Введіть ${length} чисел через кому:`);
 
+let newArray = elements.split(",").map(Number);
 
+alert(`Ваш введений масив: ${newArray}`); 
 
-let array = [];
+let sortedArray = newArray.sort((a, b) => a - b);
 
+alert(`Масив відсортовано за зростанням: ${sortedArray}`);
+
+let deletedArray = sortedArray.slice(1,3);
+alert(`Елеменети з 2 по 4 видалено: ${deletedArray}`);
